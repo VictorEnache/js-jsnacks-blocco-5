5318bd3c48139468d139a6a3065e6d30ef75f55f
VictorEnache <victor22enache@gmail.com>
2022-04-12 12:18:48 +0200

    es 4

---
+++ snack_4/main.js
@@ -0,0 +1,14 @@
+/* Scrivi una funzione che fonda due array 
+(aventi lo stesso numero di elementi) prendendo alternativamente 
+gli elementi da uno e dall’altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3]. */
+
+const array_nomi = ['victor', 'ginevra', 'gianni', 'carlo' ]
+const array_cognomi = ['enache', 'giolitto', 'fioriti', 'matconi']
+const new_array = []
+
+for (let i = 0; i<array_nomi.length; i++){
+new_array.push(array_nomi[i] +''+ array_cognomi[i])
+}
+
+console.log(new_array)

f2e96e9d4d0426aa8b85805e32ff04222cfbc58e
VictorEnache <victor22enache@gmail.com>
2022-04-12 12:01:41 +0200

    es 3

---
+++ snack_3/main.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa 
+come argomento e la ritorni girata (es. Ciao -> oaiC)
+ */
+
+
+function (parola_da_rovesciare){
+    const parola = parola_da_rovesciare
+
+let parola_rovesciata = ''
+
+for (let i = parola.length - 1; i>=0; i--){
+    const lettera = parola[i]
+    parola_rovesciata += lettera
+}
+
+console.log(parola_rovesciata)
+}
